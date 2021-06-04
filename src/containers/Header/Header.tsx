88b3b9deb3af6abb4008a9fc53a283c0c07f8b4b
KostyaNazvanov <k.nazvanov@theevenstar.com>
2021-06-04 15:44:34 +0300

    Add icon to header

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Logo from '../../assets/Logo';
 import './Header.css';
 
 export default () => {
   return (
     <div className="header">
-      <div className="header__logo">Logo</div>
+      <div className="header__logo"><Logo/></div>
       <div className="header__user-menu">User-Menu</div>
     </div>
   )
